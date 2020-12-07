c7af9fd4e2ebfa50d64e2ea859ced66e53b5baea
Chris Paul <cpaul@squareup.com>
2020-12-06 22:37:24 -0800

    Clean up comments

--- day7.rb
+++ day7.rb
@@ -59,16 +59,18 @@ def children(target, rules)
 end
 
 # part 2
-input = [
-  'dark red bags contain 2 dark orange bags.',
-  'dark orange bags contain 2 dark yellow bags.',
-  'dark yellow bags contain 2 dark green bags.',
-  'dark green bags contain 2 dark blue bags.',
-  'dark blue bags contain 2 dark violet bags.',
-  'dark violet bags contain no other bags.',
-  'shiny gold bags contain 2 dark red bags.',
-]
-rules = parse(input)
-children('shiny gold', rules) - 1 # 126
+# input = [
+#   'dark red bags contain 2 dark orange bags.',
+#   'dark orange bags contain 2 dark yellow bags.',
+#   'dark yellow bags contain 2 dark green bags.',
+#   'dark green bags contain 2 dark blue bags.',
+#   'dark blue bags contain 2 dark violet bags.',
+#   'dark violet bags contain no other bags.',
+#   'shiny gold bags contain 2 dark red bags.',
+# ]
+# rules = parse(input)
+# children('shiny gold', rules) - 1 # 126
 
-# input = File.readlines('day7-input.txt').map(&:strip)
+input = File.readlines('day7-input.txt').map(&:strip)
+rules = parse(input)
+children('shiny gold', rules) - 1 # minus the bag itself
